9ee3f00dfb5191a5f79201a77d5ebf68b8f00b78
Olga Karpenko <okarpenko@usfca.edu>
2024-02-08 08:23:34 -0800

    Added an exercise on sorting an array of 0s and 1st.

---
+++ src/main/java/Feb8Sorting01Exercise.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Feb8Sorting01Exercise {
+
+    /**
+     * Sort the given array of 0s and 1s in linear time and one pass
+     * @param arr input array
+     */
+    public static void sort01(int[] arr) {
+        // FILL IN CODE
+        int i = 0; // starts at the beginning
+        int j = arr.length - 1; // starts at the end
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1};
+        System.out.println(Arrays.toString(arr));
+        sort01(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
