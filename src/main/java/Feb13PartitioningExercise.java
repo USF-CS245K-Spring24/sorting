9038625a5ad7cb0ce1c56f1daf211aef471fb399
Olga Karpenko <okarpenko@usfca.edu>
2024-02-12 22:45:46 -0800

    Added an exercise on partitioning the array using the pivot.

---
+++ src/main/java/Feb13PartitioningExercise.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Feb13PartitioningExercise {
+    /**
+     * Pick the middle element of the array as the pivot.
+     * Partition the input array (in linear time, in one pass) so that elements on the left of the pivot are smaller than pivot,
+     * and elements that are larger than pivot (or equal to pivot) are on the right side of the pivot
+     * @param arr input array
+     */
+    public static void partitionArray(int[] arr) {
+        int n = arr.length;
+        int midIndex = n / 2;
+        int pivotElement = arr[midIndex];
+        int i = 0;
+        int j = n - 2;
+
+        // FILL IN CODE
+    }
+
+    public static void main(String[] args) {
+        int[] arr2 = {5, 9, 0, 1, 12, 6, 20, 4, 10, 3, 45};
+        System.out.println(Arrays.toString(arr2));
+        partitionArray(arr2); // 6 is the pivot in this example
+        System.out.println(Arrays.toString(arr2));
+
+    }
+}
