e304b8d2c5ff403e0a9751cc7578f49f106be5ca
Olga Karpenko <okarpenko@usfca.edu>
2024-02-15 12:20:29 -0800

    Updated the counting sort exercise and added the solution.

--- src/main/java/CountingSortExerciseSolution.java
+++ src/main/java/CountingSortExerciseSolution.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class CountingSortExercise {
+public class CountingSortExerciseSolution {
     /**
      * @param arr input array
      * Assume the range of elements  in the array is from 0 to maxValue
@@ -10,11 +10,22 @@ public class CountingSortExercise {
     public static void basicCountingSort(int[] arr, int maxValue) {
         int[] counterArray = new int[maxValue + 1];
         // Iterate over arr, and for each val = arr[i], increment counterArray[val].
-        // FILL IN CODE
+        for (int i = 0; i < arr.length; i++) {
+            int elem =  arr[i];
+            counterArray[elem]++;
+        }
+        System.out.println(Arrays.toString(counterArray));
         // Iterate over the counter array, and use it to sort arr
-        // FILL IN CODE
-        // write a loop that adds k to arr num=counterArray[k] times
-        // FILL IN CODE
+        int j = 0;
+        int k = 0; // index that keeps track of where we are at in the arr
+        while (j < counterArray.length) {
+            int frequency = counterArray[j];
+            for (int l = 0; l < frequency; l++) {
+                arr[k] = j;
+                k++;
+            }
+            j++;
+        }
 
     }
 
@@ -26,4 +37,3 @@ public class CountingSortExercise {
     }
 
 }
-
